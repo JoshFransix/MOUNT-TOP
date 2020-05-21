4740500192a9f529374837b1a4fdff0acd8b316e
Josh Anderson <joshiandersonk69@gmail.com>
2020-05-21 13:38:02 +0100

    mount top

---
+++ main.js
@@ -0,0 +1,43 @@
+// Initialize and add the Map
+function initMap() {
+  const loc = {lat: 6.455939, lng: 3.288919};
+  // Centered Map On Location
+  const map = new google.maps.Map(document.querySelector('.map')
+  , {
+      zoom:14,
+      center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({position: loc, map:
+  map });
+}
+// Olute, Lagos, Nigeria
+
+// Sticky Menu Background
+window.addEventListener('scroll', function() {
+
+  if (window.scrollY > 150) {
+      document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+      document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+// Smooth scrolling
+$('#navbar a, .btn').on('click', function(event) {
+
+  if (this.hash !== '') {
+
+   event.preventDefault();
+
+   const hash = this.hash;
+
+   $('html,body').animate(
+       {
+           scrollTop: $(hash).offset().top - 100
+       },
+       700
+   );
+}
+});
+        
